d4147cb39a34ff27f897bb8d243a30b4f949c6f4
Mark Nicholls <mnicholl@opentext.com>
2016-10-26 13:09:42 -0400

    fixing path

--- lib/lita-api-ai.rb
+++ lib/lita-api-ai.rb
@@ -4,7 +4,7 @@ Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
 )]
 
-require "/home/mark/Documents/cookbooks/lita-api-ai/lib/lita/handlers/api_ai.rb"
+require "lita/handlers/api_ai.rb"
 
 Lita::Handlers::ApiAi.template_root File.expand_path(
   File.join("..", "..", "templates"),
